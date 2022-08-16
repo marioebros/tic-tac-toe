bb237c3481b524d5cb9619ad37059f7666f696d9
Mario Elizaldegit config --global user.email dollarize.mme@gmail.com <dollarize.mme@gmail.com>
2022-08-15 17:17:24 -0700

    game interactivity added

--- main.js
+++ main.js
@@ -34,3 +34,52 @@ function startGame() {
   setBoardHoverClass();
   winningMessageElement.classList.remove("show");
 }
+
+function handleCellClick(e) {
+  const cell = e.target;
+  const currentClass = isPlayer_O_Turn ? PLAYER_O_CLASS : PLAYER_X_CLASS;
+  placeMark(cell, currentClass);
+  if (checkWin(currentClass)) {
+    endGame(false);
+  } else if (isDraw()) {
+    endGame(true);
+  } else {
+    swapTurns();
+    setBoardHoverClass();
+  }
+}
+
+function endGame(draw) {
+  if (draw) {
+    winningMessageTextElement.innerText = "Issa draw!";
+  } else {
+    winningMessageTextElement.innerText = `Player with ${
+      isPlayer_O_Turn ? "O's" : "X's"
+    } wins!`;
+  }
+  winningMessageElement.classList.add("show");
+}
+
+function isDraw() {
+    return [...cellElements].every(cell => {
+        return.cell.classList.contains(PLAYER_X_CLASS) || cell.classList.contains(PLAYER_O_CLASS);
+    })
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass);
+}
+
+function swapTurns() {
+    isPlayer_O_Turn = !isPlayer_O_Turn;
+}
+
+function setBoardHoverClass() {
+    boardElement.classList.remove(PLAYER_X_CLASS);
+    boardElement.classList.remove(PLAYER_O_CLASS);
+    if (isPlayer_O_Turn) {
+        boardElement.classList.add(PLAYER_O_CLASS);
+    } else {
+        boardElement.classList.add(PLAYER_X_CLASS);
+    }
+}
